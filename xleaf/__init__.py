3848afd693946755cbf93c2d899f8571617ce323
christopher anderson <cbanders@stanford.edu>
2023-02-27 13:38:34 -0800

    v1.0 exposes more parameters to users

--- xleaf/__init__.py
+++ xleaf/__init__.py
@@ -1,3 +1,27 @@
-from xleaf import parameters
+from xleaf import parameters, samplers
 from xleaf.__version__ import __doc__ as __version__
+from xleaf.parameters import (
+    AnthocyaninSampler,
+    CarotenoidSampler,
+    ChlorophyllSampler,
+    EWTSampler,
+    LAICropSampler,
+    LAIForestSampler,
+    LeafErectophile,
+    LeafPlagiophile,
+    LeafPlanophile,
+    LeafSpherical,
+    LeafUniform,
+    LMASampler,
+    NormalSampler,
+    NSampler,
+    SoilDrynessSampler,
+    SolarAzimuthSampler,
+    SolarZenithSampler,
+    UniformSampler,
+    ViewAzimuthSampler,
+    ViewZenithSampler,
+    fwhms,
+    wavelengths,
+)
 from xleaf.simulators import simulate_canopy, simulate_leaf
